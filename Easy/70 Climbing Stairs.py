14d1da78260cb412c3f8cdd40fbae1970bbc99a4
Sethu-bharathi <bharathisethu018@gmail.com>
2021-09-07 06:56:23 +0530

    70 completed

---
+++ Easy/70 Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if(n<=2):
+            return n
+        else:
+            a = [0 for i in range(n+1)]
+            a[1] = 1
+            a[2] = 2
+
+            for i in range(3,n+1):
+                a[i]=a[i-1]+a[i-2]
+            return a[n]
+n=int(input("enter the number of stairs"))
+stairs1=Solution()
+print(stairs1.climbStairs(n))
