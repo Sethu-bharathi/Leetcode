657967ae0a8601e4278ab67a1fed72d394d05e61
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-01-28 20:01:18 +0530

    Time: 109 ms (6.24%), Space: 13.8 MB (99.99%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,29 +1,15 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        d={1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        res=""
+        for i in d:
+            quo,num=divmod(num,i)
+            if quo>0:
+                res+=quo*d[i]
+            if num==0:
+                break
+        return res
+                
+
+            
         
-        dic = {
-            1:'I',
-            4:'IV',
-            5:'V',
-            9:'IX',
-            10:'X',
-            40:'XL',
-            50:'L',
-            90:'XC',
-            100:'C',
-            400:'CD',
-            500:'D',
-            900:'CM',
-            1000:'M'}
-        
-        
-        numbers = list(dic.keys())[::-1]
-        
-        ans = []
-        for nd in numbers:
-            div1, mod1 = divmod(num, nd)
-            if div1 > 0:
-                ans.append(div1*dic[nd])
-            num = mod1
-        
-        return ''.join(ans)
