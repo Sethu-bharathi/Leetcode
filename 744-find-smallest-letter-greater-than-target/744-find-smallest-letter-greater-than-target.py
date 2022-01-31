a8607dbbb0ed3c2fc9b597e958b65b7a5cfa03a3
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-01-31 10:22:33 +0530

    Time: 206 ms (10.74%), Space: 14.4 MB (97.02%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -2,11 +2,12 @@ class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         left=0
         right=len(letters)-1
+        fn=ord
         while left<=right:
             mid=left+(right-left)//2
-            if ord(letters[mid])>ord(target) and ord(letters[mid-1])<=ord(target):
+            if fn(letters[mid])>fn(target) and fn(letters[mid-1])<=fn(target):
                 return letters[mid]
-            elif ord(letters[mid])>ord(target) and ord(letters[mid-1])>ord(target):
+            elif fn(letters[mid])>fn(target) and fn(letters[mid-1])>fn(target):
                 right=mid-1
             else:
                 left=mid+1
