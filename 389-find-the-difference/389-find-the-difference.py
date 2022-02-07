66b5f0d18179d720102c6cf896cfcfbbcb11cc97
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-02-07 19:00:54 +0530

    Time: 36 ms (76.52%), Space: 13.9 MB (88.02%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        fn=ord
+        x=fn(t[0])
+        for i in t[1:]:
+            x^=fn(i)
+        for i in s:
+            x^=fn(i)
+        return (chr(x))
