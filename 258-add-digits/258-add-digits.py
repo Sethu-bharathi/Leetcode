ef32713843efdfd4d3d95062b9b4c6df5d1925ac
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-02-08 07:17:13 +0530

    Time: 46 ms (41.75%), Space: 13.8 MB (91.66%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,13 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num<10:
-            return num
-        count=0
-        while num>9:
-            count+=num%10
-            num//=10
-        count+=num
-        if count>=10:
-            return self.addDigits(count)
-        return count
+        if num==0:
+            return 0
+        if num%9==0:
+            return 9
+        return num%9
         
