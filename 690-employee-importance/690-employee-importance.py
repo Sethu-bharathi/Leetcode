f23d1517112408592ea8b0df0cb2ce9cc275fae5
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-02-22 18:25:14 +0530

    Time: 184 ms (60.77%), Space: 15.4 MB (89.73%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.py
@@ -0,0 +1,17 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution(object):
+    def getImportance(self, employees, query_id):
+        emap = {e.id: e for e in employees}
+        def dfs(eid):
+            employee = emap[eid]
+            return (employee.importance +
+                    sum(dfs(eid) for eid in employee.subordinates))
+        return dfs(query_id)
