9c9e278cbf65962b1ab5bc4b1cbd594e6b60aba1
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-02-28 23:46:50 +0530

    Time: 66 ms (16.12%), Space: 13.8 MB (82.95%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool: 
+        def endless(n, l):
+            if n in l:
+                return False
+            if n == 1:
+                return True
+            l[n]=1
+            running_sum = 0
+            while n > 0:
+                running_sum += pow(n%10,2)
+                n = n // 10
+            
+            return endless(running_sum,l)
+        return endless(n,{})
+        
+        
