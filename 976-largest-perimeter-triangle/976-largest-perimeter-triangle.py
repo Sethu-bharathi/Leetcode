eb352782156f232539a1b01df08312407c2ca3b8
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-03-01 07:53:41 +0530

    Time: 318 ms (31.23%), Space: 15.5 MB (68.15%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -2,7 +2,7 @@ class Solution:
     def isTriangle(self,a,b,c):
         return a+b>c and b+c>a and a+c>b
     def largestPerimeter(self, nums: List[int]) -> int:
-        nums.sort()
+        nums=sorted(nums)
         for i in range(len(nums)-1,1,-1):
             if self.isTriangle(nums[i],nums[i-1],nums[i-2]):
                 return nums[i-2]+nums[i-1]+nums[i]
