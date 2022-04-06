9fa1131240b2e7bdc43fc57fec38e54df376db6e
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-04-06 18:53:23 +0530

    Time: 51 ms (34.43%), Space: 14 MB (12.52%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,4 +1,9 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        d=collections.Counter(sentence)
-        return len(d)==26
+        count=0
+        d={}
+        for i in sentence:
+            if i not in d:
+                count+=1
+                d[i]=1
+        return count==26
