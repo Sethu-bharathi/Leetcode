09187a89d4fbc466f64774e340d2b3aafd6fd6df
Sethu-bharathi <67668741+Sethu-bharathi@users.noreply.github.com>
2022-05-22 10:41:51 +0530

    Time: 2271 ms (33.33%), Space: 59.5 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.py
@@ -3,7 +3,7 @@ class Solution:
         def slope(x1,x2,y1,y2):
             return (y2-y1),(x2-x1)
         
-        stockprices.sort()
+        stockprices=sorted(stockprices)
         n=len(stockprices)
         if n==1:
             return 0  
@@ -12,7 +12,6 @@ class Solution:
             x1,y1=stockprices[i-1]
             x2,y2=stockprices[i]
             x3,y3=stockprices[i+1]
-            
             if (y1-y2)*(x2-x3)!=(y2-y3)*(x1-x2):
                 count+=1
         return count
